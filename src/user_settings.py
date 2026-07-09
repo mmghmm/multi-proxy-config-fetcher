899886b3f00c1572f7f70cae6b1bbb5db9dbc50f
mmghmm <126723510+mmghmm@users.noreply.github.com>
2026-07-09 12:55:37 +0330

    Remove unused URLs from SOURCE_URLS
    Removed several URLs from the SOURCE_URLS list in user_settings.py.

--- src/user_settings.py
+++ src/user_settings.py
@@ -5,23 +5,16 @@
 SOURCE_URLS = [
     "https://raw.githubusercontent.com/mahsanet/MahsaFreeConfig/refs/heads/main/mtn/sub_1.txt",
     "https://t.me/s/persianvpnhub",
-    "https://t.me/s/v2rayfree",
     "https://t.me/s/PrivateVPNs",
     "https://t.me/s/prrofile_purple",
     "https://t.me/s/DirectVPN",
     "https://raw.githubusercontent.com/MahsaNetConfigTopic/config/refs/heads/main/xray_final.txt",
     "https://raw.githubusercontent.com/Mahdi0024/ProxyCollector/master/sub/proxies.txt",
-    "https://raw.githubusercontent.com/arshiacomplus/v2rayExtractor/refs/heads/main/mix/sub.html",
     "https://raw.githubusercontent.com/parvinxs/Submahsanetxsparvin/refs/heads/main/Sub.mahsa.xsparvin",
-    "https://raw.githubusercontent.com/Freedom-Guard-Builder/FL/refs/heads/main/config/Fast.txt",
     "https://raw.githubusercontent.com/Ashkan-m/v2ray/main/Sub.txt",
     "https://raw.githubusercontent.com/davudsedft/purvpn/refs/heads/main/links/purkow.txt",
     "https://t.me/s/v2ray_free_conf",
-    "https://t.me/s/IP_CF_Config",
-    "https://t.me/s/vpnfail_vless",
-    "https://t.me/s/DailyV2RY",
     "https://t.me/s/moftconfig",
-    "https://t.me/s/ConfigsHUB2",
     # Add more URLs here if you want to include additional sources.
 ]
 
